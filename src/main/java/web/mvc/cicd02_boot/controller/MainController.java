7e10ddad6261fcc001a277ba15bd39014a4e1c88
miyeon <myksphone2001@gmail.com>
2026-05-29 16:11:39 +0900

    application.properties upload

--- src/main/java/web/mvc/cicd02_boot/controller/MainController.java
+++ src/main/java/web/mvc/cicd02_boot/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String index(){
-        return "GitHub Actions를 이용한 CI/CD 테스트입니다 - 자동배포중!!!";
+        return "GitHub Actions를 이용한 CI/CD 테스트입니다 - application 설정도 secret key로 정의해서 사용하기!!";
     }
 }
