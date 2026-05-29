1102167baf104c6b6dc2b7909ff3c110e63f5843
miyeon <myksphone2001@gmail.com>
2026-05-29 15:43:24 +0900

    자동화 배포

--- src/main/java/web/mvc/cicd02_boot/controller/MainController.java
+++ src/main/java/web/mvc/cicd02_boot/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String index(){
-        return "GitHub Actions를 이용한 second CI/CD TEST - 수동 배포";
+        return "GitHub Actions를 이용한 CI/CD 테스트입니다 - 자동배포중!!!";
     }
 }
