c190a6744ea6073e70f835fb2ff42dc2cf59799e
miyeon <myksphone2001@gmail.com>
2026-05-29 16:18:59 +0900

    test 실패

--- src/main/java/web/mvc/cicd02_boot/controller/MainController.java
+++ src/main/java/web/mvc/cicd02_boot/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String index(){
-        return "GitHub Actions를 이용한 CI/CD 테스트입니다 - application 설정도 secret key로 정의해서 사용하기!!";
+        return "GitHub Actions를 이용한 CI/CD 테스트입니다 - 배포 실패하기!!";
     }
 }
